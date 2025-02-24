2f18be71ed2f6ccf14be3e86664b0d9e4a5ed87b
martinseijo <martinseijoperez@gmail.com>
2025-02-24 21:48:54 +0100

    Fix Swagger

--- src/main/java/com/martinseijo/spaceship/infrastructure/exception/GlobalExceptionHandler.java
+++ src/main/java/com/martinseijo/spaceship/infrastructure/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.martinseijo.spaceship.infrastructure.exception;
 
 import com.martinseijo.spaceship.domain.exception.InvalidInputException;
 import com.martinseijo.spaceship.domain.exception.ResourceNotFoundException;
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
+@Hidden
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
